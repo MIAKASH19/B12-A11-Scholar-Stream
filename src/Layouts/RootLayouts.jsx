e7ede50a43216a139a5219904d63943a8bbd087e
Mi Akash <mszharul1990@gmail.com>
2025-12-19 03:08:09 +0600

    Scroll Issue Fixed

--- src/Layouts/RootLayouts.jsx
+++ src/Layouts/RootLayouts.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import Navbar from '../Components/Navbar'
-import { Outlet } from 'react-router'
-import Footer from '../Components/Footer'
+import React from "react";
+import Navbar from "../Components/Navbar";
+import { Outlet } from "react-router";
+import Footer from "../Components/Footer";
+import ScrollToTop from "../Components/ScrollToTop";
 
 const RootLayouts = () => {
   return (
-    <div className='font-inter'>
-        <Navbar></Navbar>
-        <Outlet></Outlet>
-        <Footer></Footer>
+    <div className="font-inter">
+      <ScrollToTop />
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
     </div>
-  )
-}
+  );
+};
 
-export default RootLayouts
+export default RootLayouts;
