d2bdf660e4959215b13acfc555643cc43c8f0fd9
Mi Akash <mszharul1990@gmail.com>
2025-12-20 19:21:53 +0600

    Filter System added

--- src/Layouts/RootLayouts.jsx
+++ src/Layouts/RootLayouts.jsx
@@ -6,7 +6,7 @@ import ScrollToTop from "../Components/ScrollToTop";
 
 const RootLayouts = () => {
   return (
-    <div className="font-inter">
+    <div className="font-inter ">
       <ScrollToTop />
       <Navbar></Navbar>
       <Outlet></Outlet>
--- src/Pages/AllScholarships.jsx
+++ src/Pages/AllScholarships.jsx
@@ -2,13 +2,19 @@ import { motion } from "framer-motion";
 import React, { useEffect, useState } from "react";
 import ScholarCard from "../Components/ScholarCard";
 import useAxiosSecure from "../Hooks/useAxiosSecure";
+import { IoSearch } from "react-icons/io5";
+import { GoSearch } from "react-icons/go";
 
 const AllScholarships = () => {
   const [scholarships, setScholarships] = useState([]);
   const axiosSecure = useAxiosSecure();
+  const [countries, setCountries] = useState([]);
   const [totalScholarships, setTotalScholarships] = useState(0);
   const [currentPage, setCurrentPage] = useState(0);
   const [totalPages, setTotalPages] = useState(0);
+  const [category, setCategory] = useState("");
+  const [location, setLocation] = useState("");
+
   const limit = 6;
 
   useEffect(() => {
@@ -16,22 +22,27 @@ const AllScholarships = () => {
       .get(
         `http://localhost:3000/scholarships?limit=${limit}&skip=${
           currentPage * limit
-        }`
+        }&category=${category}&location=${location}`
       )
       .then((res) => {
         setScholarships(res.data.result);
         setTotalScholarships(res.data.total);
         const pages = Math.ceil(res.data.total / limit);
         setTotalPages(pages);
-        // console.log(pages)
       })
       .catch((error) => {
         console.error("Failed to fetch scholarships:", error);
       });
-  }, [currentPage]);
+  }, [currentPage, category, location]);
+
+  useEffect(() => {
+    axiosSecure
+      .get("/scholarship-countries")
+      .then((res) => setCountries(res.data));
+  }, []);
 
   return (
-    <div className="max-w-7xl relative  mx-auto px-4 py-20 pt-30 overflow-hidden">
+    <div className="w-full relative mx-auto px-10 py-20 pt-30 ">
       <div className="bg-blue-200 w-100 h-70 absolute top-80 left-0 blur-2xl scale-200 rounded-full -z-1" />
       <div className="bg-pink-100 w-100 h-70 absolute top-80 right-0 blur-2xl scale-200 rounded-full -z-1" />
       <motion.div
@@ -43,13 +54,55 @@ const AllScholarships = () => {
         <h1 className="text-4xl md:text-5xl font-medium mb-4 text-gray-800">
           Explore All Scholarships
         </h1>
-        <p className="text-gray-600 max-w-6xl mx-auto text-sm">
+        <p className="text-gray-600 max-w-5xl mx-auto text-sm">
           Discover verified local and international scholarships to fund your
           education. Browse through our curated collection and find
           opportunities that match your degree, subject, and career goals. Apply
           directly from here and never miss a deadline!
         </p>
       </motion.div>
+
+      <div className="mb-12">
+        <div className="flex flex-col md:flex-row gap-4 items-center justify-between">
+          <h1 className="text-2xl ">Total ({totalScholarships})</h1>
+          {/* Search Input */}
+          <div className="relative w-full md:w-[50%]">
+            <input
+              type="text"
+              placeholder="Search by scholarship, university or degree..."
+              className="w-full rounded-xl border border-zinc-300 bg-white px-5 py-3 text-sm outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-100 transition"
+            />
+            <span className="absolute right-4 top-1/2 -translate-y-1/2 text-zinc-600 text-lg">
+              <GoSearch />
+            </span>
+          </div>
+
+          {/* Filter / Sort Dropdown */}
+          <div className="flex gap-3 w-full md:w-auto">
+            <select
+              onChange={(e) => setCategory(e.target.value)}
+              className="w-full md:w-[200px] rounded-xl border border-zinc-300 bg-white px-2 py-3 text-sm outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-100 transition"
+            >
+              <option value="">Scholarship Category</option>
+              <option value="Full Fund">Full Fund</option>
+              <option value="Partial Fund">Partial Fund</option>
+              <option value="Self Fund">Self Fund</option>
+            </select>
+
+            <select
+              onChange={(e) => setLocation(e.target.value)}
+              className="w-full md:w-[200px] rounded-xl border border-zinc-300 bg-white px-2 py-3 text-sm outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-100 transition"
+            >
+              <option value="">Location</option>
+              {countries.map((country) => (
+                <option key={country} value={country}>
+                  {country}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+      </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {scholarships.map((scholarship, index) => (
@@ -62,7 +115,10 @@ const AllScholarships = () => {
       </div>
       <div className="mt-10  flex items-center justify-center gap-3">
         {currentPage > 0 && (
-          <button onClick={()=> setCurrentPage(currentPage -1)} className="btn border border-zinc-300 bg-white   hover:bg-blue-600 hover:border-blue-600">
+          <button
+            onClick={() => setCurrentPage(currentPage - 1)}
+            className="btn border border-zinc-300 bg-white   hover:bg-blue-600 hover:border-blue-600"
+          >
             Prev
           </button>
         )}
@@ -80,7 +136,10 @@ const AllScholarships = () => {
           </button>
         ))}
         {currentPage < totalPages - 1 && (
-          <button onClick={()=> setCurrentPage(currentPage +1)} className="btn border border-zinc-300 bg-white   hover:bg-blue-600 hover:border-blue-600">
+          <button
+            onClick={() => setCurrentPage(currentPage + 1)}
+            className="btn border border-zinc-300 bg-white   hover:bg-blue-600 hover:border-blue-600"
+          >
             next
           </button>
         )}
